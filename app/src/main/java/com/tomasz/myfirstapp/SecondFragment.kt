74c0da423c9ca8f393cd4e931b501257ef36538f
tomaszpawelek213 <tomaszpawelek213@gmail.com>
2020-12-14 20:10:03 +0100

    First screen added

---
+++ app/src/main/java/com/tomasz/myfirstapp/SecondFragment.kt
@@ -0,0 +1,16 @@
+package com.tomasz.myfirstapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
