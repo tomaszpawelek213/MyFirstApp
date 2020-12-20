00f82788eca3db86c7701aa5eaa18466c775fc2e
tomaszpawelek213 <tomaszpawelek213@gmail.com>
2020-12-20 20:05:09 +0100

    login page

--- app/src/main/java/com/tomasz/myfirstapp/LiveDataUtils.kt
+++ app/src/main/java/com/tomasz/myfirstapp/LiveDataUtils.kt
@@ -1,7 +1,11 @@
 package com.tomasz.myfirstapp
 
+import androidx.lifecycle.LiveData
 import com.hadilq.liveevent.LiveEvent
 
 fun LiveEvent<Unit>.fireEvent() {
     value = Unit
 }
+
+inline val <T> LiveData<T>.valueNN
+    get() = value!!
