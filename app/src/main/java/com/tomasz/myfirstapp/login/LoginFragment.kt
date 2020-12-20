4283e0544e497c57a9bf935f0d537a5f22f58a8a
tomaszpawelek213 <tomaszpawelek213@gmail.com>
2020-12-20 20:05:09 +0100

    login page

---
+++ app/src/main/java/com/tomasz/myfirstapp/login/LoginFragment.kt
@@ -0,0 +1,51 @@
+package com.tomasz.myfirstapp.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.observe
+import com.tomasz.myfirstapp.BR
+import com.tomasz.myfirstapp.R
+import com.tomasz.myfirstapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    val viewModel: LoginViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login, container, false
+        ).apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, this@LoginFragment.viewModel)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.onValidLoginEvent.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), "zalogowano", Toast.LENGTH_LONG).show()
+        }
+        viewModel.onInvalidLoginEvent.observe(viewLifecycleOwner){
+            Toast.makeText(requireContext(), "nieporawane dane logowania: " +
+                    "[username=${viewModel.username.value}, password=${viewModel.password.value}]",
+                Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/tomasz/myfirstapp/login/LoginViewModel.kt
@@ -0,0 +1,39 @@
+package com.tomasz.myfirstapp.login
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.hadilq.liveevent.LiveEvent
+import com.tomasz.myfirstapp.fireEvent
+import com.tomasz.myfirstapp.valueNN
+
+class LoginViewModel : ViewModel() {
+    val username = MutableLiveData<String>("")
+    val password = MutableLiveData<String>("")
+
+    val onValidLoginEvent = LiveEvent<Unit>()
+    val onInvalidLoginEvent = LiveEvent<Unit>()
+
+    fun onLoginButtonClick() {
+        if (username.value == "admin" && password.value == "Admin123" && isPasswordValid()) {
+            onValidLoginEvent.fireEvent()
+        } else {
+            onInvalidLoginEvent.fireEvent()
+        }
+    }
+
+    fun isPasswordValid(): Boolean {
+        if (password.valueNN.length < 8) {
+            return false
+        }
+
+//        for (c in password.valueNN.toCharArray()) {
+//            if (c.isUpperCase()) {
+//                return true
+//            }
+//        }
+
+        return false;
+    }
+
+}
+
